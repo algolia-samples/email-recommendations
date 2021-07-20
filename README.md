10b0ad56be68fc12c8e93c6ddca295dd3a916a40
Clement Denoix <flydesigned@gmail.com>
2021-07-20 08:08:10 +0200

    feat: Add email sending feature

--- README.md
+++ README.md
@@ -58,7 +58,19 @@ ALGOLIA_INDEX_NAME=<replace-with-your-algolia-index-name>
 ALGOLIA_API_KEY=<replace-with-your-algolia-api-key>
 ```
 
-### 6. Follow the instructions in the server directory
+### 4. (Optional) Configure an email sending provider
+
+The sample application allow you to send the preview to your email address using an email sending provider.
+We do provide a minimal working exemple with [SendGrid](https://sendgrid.com/).
+
+In order to use SendGrid, you need to set the environment variables `SENDGRID_API_KEY` and `SENDGRID_FROM_EMAIL` in the `.env` file:
+
+```bash
+SENDGRID_API_KEY=<replace-with-your-sendgrid-api-key>
+SENDGRID_FROM_EMAIL=<replace-with-sendgrid-from-email>
+```
+
+### 5. Follow the instructions in the server directory
 
 Each server directory has a file with instructions:
 
--- server/node/server.js
+++ server/node/server.js
@@ -3,14 +3,16 @@ import fs from "fs";
 import { fileURLToPath } from "url";
 import path from "path";
 
-import bodyParser from "body-parser";
 import dotenv from "dotenv"
 import express from "express";
 import nunjucks from "nunjucks";
 import showdown from "showdown";
+import sendgrid from "@sendgrid/mail";
 
 import algoliaearch from "algoliasearch";
 import algoliarecommend from "@algolia/recommend";
+
+import sendEmail from "./sendgrid.js";
 
 // Load env variables
 const __filename = fileURLToPath(import.meta.url);
@@ -25,6 +27,9 @@ const algoliaClient = algoliaearch(process.env.ALGOLIA_APP_ID, process.env.ALGOL
 const recommendClient = algoliarecommend(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_API_KEY);
 const algoliaIndex = algoliaClient.initIndex(process.env.ALGOLIA_INDEX_NAME);
 
+// Setup email delivery
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+
 // Email templates rendering engine
 // https://mozilla.github.io/nunjucks/
 nunjucks.configure('templates', {
@@ -33,7 +38,7 @@ nunjucks.configure('templates', {
 });
 
 app.use(express.static(process.env.STATIC_DIR));
-app.use(bodyParser.json())
+app.use(express.json());
 
 const loadEmails = async () => {
     const EmailBaseDir = "./emails";
@@ -51,6 +56,17 @@ const loadEmails = async () => {
     };
     return emails;
 };
+
+const renderEmail = async (email) => {
+    // Load all products recommendations to populate the email template.
+    const recommendations = await Promise.all(
+        email.recommendations.map(f => f(algoliaIndex, recommendClient))
+    );
+    return nunjucks.render(email.template, {
+        ...email,
+        recommendations: recommendations,
+    });
+}
 
 
 // Home page
@@ -76,18 +92,25 @@ app.get("/render/:emailIndex", async (req, res) => {
     const { emailIndex } = req.params;
     const emails = await loadEmails();
     const email = emails[emailIndex];
+    const renderedEmail = await renderEmail(email);
 
-    // Load all products recommendations to populate the email template.
-    const recommendations = await Promise.all(
-        email.recommendations.map(f => f(algoliaIndex, recommendClient))
-    );
     res.json({
         'explanation': email.explanation,
-        'html': nunjucks.render(email.template, {
-            ...email,
-            recommendations: recommendations,
-        })
+        'html': renderedEmail
     });
+});
+
+// Send a given email, identified by his index
+app.post("/send/:emailIndex", async (req, res) => {
+    const { emailIndex } = req.params;
+    const emails = await loadEmails();
+    const email = emails[emailIndex];
+    const renderedEmail = await renderEmail(email);
+
+    const { toEmail } = req.body;
+    await sendEmail(toEmail, email.title, "test", renderedEmail);
+    
+    res.status(200);
 });
 
 app.listen(4242, () => console.log(`Node server listening on port ${4242}!`));
